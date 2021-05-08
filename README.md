168b6c7d7bce961176065ee8717d4ae27a5bb8ec
NyCode <nico@nycode.de>
2021-05-08 19:02:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# wiki
+Small wiki written in Go.
+
+This application is based off [this](https://golang.org/doc/articles/wiki/) GoLang Documentation Article.
+
+I am still a beginner in GoLang, feel free to submit tips and suggestions. :)
---
+++ page.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := "data/" + p.Title + ".txt"
+
+	_, err := ioutil.ReadDir("data")
+	if err != nil {
+		err = os.Mkdir("data", 0600)
+		if err != nil {
+			return err
+		}
+	}
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := "data/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
